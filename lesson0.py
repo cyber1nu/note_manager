fb339cfc63f1c9ffd219d82de7c908afac362fab
cyber1nu <111985477+cyber1nu@users.noreply.github.com>
2025-01-14 21:17:40 +0300

    first homework

--- lesson0.py
+++ lesson0.py
@@ -1,1 +1,15 @@
-print('Hello world!')
+username = 'cyber1nu'
+title = 'Задача №1'
+content = 'Выполнить первое домашнее задание'
+status = False
+created_date = '14-01-2025'
+issue_date = '15-01-2025'
+
+print(
+    'Имя пользователя:', username, '\n',
+    'Название задачи:', title, '\n',
+    'Описание задачи:', content, '\n',
+    'Статус:', status, '\n',
+    'Дата создания:', created_date, '\n',
+    'Конечная дата выполнения:', issue_date,
+)
