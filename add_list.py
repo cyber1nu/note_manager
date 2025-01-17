c28d143861aae847cdecff18d8822e38a6f6570a
cyber1nu <111985477+cyber1nu@users.noreply.github.com>
2025-01-17 21:06:08 +0300

    homework 3 and 4 done

---
+++ add_list.py
@@ -0,0 +1,8 @@
+title_one = input('Введите название заголовка: ')
+title_two = input('Введите название заголовка: ')
+title_three = input('Введите название заголовка: ')
+
+titles_list = [title_one, title_two, title_three]
+
+for title in titles_list:
+    print(title)
